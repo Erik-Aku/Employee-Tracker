3e314d4881f9ce3926e44fe88a089fcc975b8db9
Erik  Enoka Aku <Erik@Jennifers-iMac.lan>
2023-12-30 18:21:04 -0800

    add a role

--- server.js
+++ server.js
@@ -43,6 +43,9 @@ const mainMenu = () => {
       if (data.choices === "Add a Department") {
         addDepartment();
       }
+      if (data.choices === "Add a Role") {
+        addRole();
+      }
     });
 };
 
@@ -75,23 +78,23 @@ const viewAllRoles = () => {
 };
 
 const viewAllEmployees = () => {
-    let sql = `SELECT employee.id AS 'emp_id', employee.first_name, employee.last_name, role.title, role.salary, department.department_name, 
+  let sql = `SELECT employee.id AS 'emp_id', employee.first_name, employee.last_name, role.title, role.salary, department.department_name, 
     employee.manager_id AS 'Manager'    
     FROM department
     INNER JOIN role ON department.id = role.department_id
     INNER JOIN employee ON role.id = employee.role_id
     ORDER BY employee.id ASC`;
 
-    db.query(sql, (error, res) => {
-      if (error) {
-        throw error;
-      }
-      console.log("\n");
-      console.log(asTable(res));
-      console.log("\n");
-      mainMenu();
-    });
-}
+  db.query(sql, (error, res) => {
+    if (error) {
+      throw error;
+    }
+    console.log("\n");
+    console.log(asTable(res));
+    console.log("\n");
+    mainMenu();
+  });
+};
 
 const addDepartment = () => {
   inquirer
@@ -104,11 +107,68 @@ const addDepartment = () => {
     ])
     .then((data) => {
       let sql = `INSERT INTO department (department_name) VALUES (?)`;
-      db.query(sql, data.addDepartment, (error, res) => {
+      db.query(sql, data.addDepartment, (error) => {
         if (error) {
           throw error;
         }
         viewAllDepartments();
       });
     });
+};
+
+const addRole = () => {
+  let sql = `SELECT * from department`;
+
+  db.query(sql, (error, response) => {
+    if (error) {
+      throw error;
+    }
+
+    let existingDeptNames = [];
+
+    response.forEach((department) => {
+      existingDeptNames.push(department.department_name);
+    });
+
+    inquirer
+      .prompt([
+        {
+          name: "departmentChoice",
+          type: "list",
+          message: "Select a department for this new role.",
+          choices: existingDeptNames,
+        },
+
+        {
+          name: "newRole",
+          type: "input",
+          message: "What is the name of the new role?",
+        },
+
+        {
+          name: "salary",
+          type: "input",
+          message: "What is the salary?",
+        },
+      ])
+      .then((data) => {
+        let deptId;
+
+        response.forEach((department) => {
+          if (data.departmentChoice === department.department_name) {
+            deptId = department.id;
+          }
+        });
+
+        let sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
+        let params = [data.newRole, data.salary, deptId];
+
+        db.query(sql, params, (error) => {
+          if (error) {
+            throw error;
+          }
+          viewAllRoles();
+        });
+      });
+  });
 };
