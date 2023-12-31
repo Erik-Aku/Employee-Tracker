6dd4bf95c5f4fdda1dbcf4870381fb1da1a4bde7
Erik  Enoka Aku <Erik@Jennifers-iMac.lan>
2023-12-30 16:57:07 -0800

    mainMenu refactor, add new department

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ db.connect((error) => {
   mainMenu();
 });
 
-
 const mainMenu = () => {
   inquirer
     .prompt([
@@ -30,53 +29,86 @@ const mainMenu = () => {
     ])
     .then((data) => {
       if (data.choices === "View All Departments") {
-        let sql = `SELECT department.id AS id, department.department_name AS department FROM department`;
-        db.query(sql, (error, res) => {
-          if (error) {
-            throw error;
-          }
-          console.log("\n");
-          console.log(asTable(res));
-          console.log("\n");
-          mainMenu();
-        });
+        viewAllDepartments();
       }
 
       if (data.choices === "View All Roles") {
-        let sql = `SELECT role.id, role.title, department.department_name AS department, role.salary
-        FROM role
-        INNER JOIN department ON role.department_id = department.id`;
-        db.query(sql, (error, res) => {
-            if (error) {
-                throw error;
-            }
-            console.log("\n");
-            console.log(asTable(res));
-            console.log("\n");
-            mainMenu();
-        })
+        viewAllRoles();
       }
 
       if (data.choices === "View All Employees") {
-        let sql = `SELECT employee.id AS 'emp_id', employee.first_name, employee.last_name, role.title, role.salary, department.department_name, 
-        employee.manager_id AS 'Manager'    
-        FROM department
-        INNER JOIN role ON department.id = role.department_id
-        INNER JOIN employee ON role.id = employee.role_id
-        ORDER BY employee.id ASC`;
+        viewAllEmployees();
+      }
 
-        db.query(sql, (error, res) => {
-            if (error) {
-                throw error;
-            }
-            console.log("\n");
-            console.log(asTable(res));
-            console.log("\n");
-            mainMenu();
-        })
+      if (data.choices === "Add a Department") {
+        addDepartment();
       }
     });
 };
 
+const viewAllDepartments = () => {
+  let sql = `SELECT department.id AS id, department.department_name AS department FROM department`;
+  db.query(sql, (error, res) => {
+    if (error) {
+      throw error;
+    }
+    console.log("\n");
+    console.log(asTable(res));
+    console.log("\n");
+    mainMenu();
+  });
+};
 
+const viewAllRoles = () => {
+  let sql = `SELECT role.id, role.title, department.department_name AS department, role.salary
+        FROM role
+        INNER JOIN department ON role.department_id = department.id`;
+  db.query(sql, (error, res) => {
+    if (error) {
+      throw error;
+    }
+    console.log("\n");
+    console.log(asTable(res));
+    console.log("\n");
+    mainMenu();
+  });
+};
 
+const viewAllEmployees = () => {
+    let sql = `SELECT employee.id AS 'emp_id', employee.first_name, employee.last_name, role.title, role.salary, department.department_name, 
+    employee.manager_id AS 'Manager'    
+    FROM department
+    INNER JOIN role ON department.id = role.department_id
+    INNER JOIN employee ON role.id = employee.role_id
+    ORDER BY employee.id ASC`;
+
+    db.query(sql, (error, res) => {
+      if (error) {
+        throw error;
+      }
+      console.log("\n");
+      console.log(asTable(res));
+      console.log("\n");
+      mainMenu();
+    });
+}
+
+const addDepartment = () => {
+  inquirer
+    .prompt([
+      {
+        name: "addDepartment",
+        type: "input",
+        message: "Enter the name of the new Department",
+      },
+    ])
+    .then((data) => {
+      let sql = `INSERT INTO department (department_name) VALUES (?)`;
+      db.query(sql, data.addDepartment, (error, res) => {
+        if (error) {
+          throw error;
+        }
+        viewAllDepartments();
+      });
+    });
+};
